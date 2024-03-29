b10a02261ba6877cedb9e63801c2bad4d06c2bb9
Aadil_Tansawala <aadil882002@gmail.com>
2024-03-29 12:51:48 +0530

    changed server url

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,7 @@ const path = require("path");
 const cors = require("cors");
 
 app.use(express.json());
-app.use(cors( {
-    "origin": "*",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }  )); 
+app.options('/allproducts', cors()) 
 
 // Database Connection with MongoDB
 mongoose.connect("mongodb+srv://aadil:07070707@cluster0.x4wrsel.mongodb.net/E-COMMERCE");
