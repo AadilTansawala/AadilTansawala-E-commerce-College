ef68da4e17170e0e1498097d3ea8075e0587d46e
Aadil_Tansawala <aadil882002@gmail.com>
2024-04-01 15:14:27 +0530

    changed server url

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ app.post("/upload", upload.single('product'), (req, res) => {
     // If file upload is successful, return a JSON response with success status and image URL
     res.json({
         success: 1,
-        imageUrl: `http://${req.hostname}:${port}/images/${req.file.filename}`
+        imageUrl: `http://${req.hostname}/images/${req.file.filename}`
     });
 });
 
