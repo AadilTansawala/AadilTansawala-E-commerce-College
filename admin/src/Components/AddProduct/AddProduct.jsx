58e40631994dc78ec7da5457b68d0d1dfbba1805
Aadil_Tansawala <aadil882002@gmail.com>
2024-04-03 14:13:38 +0530

    changed uploaddir

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -17,9 +17,17 @@ const AddProduct = () => {
     };
 
     const changeHandler = (e) => {
+        // Log the input field name and value
+        console.log("Input field name:", e.target.name);
+        console.log("Input field value:", e.target.value);
+    
         // Update the productDetails state when other input fields change
         setProductDetails({ ...productDetails, [e.target.name]: e.target.value });
+    
+        // Log the updated productDetails state
+        console.log("Updated product details:", productDetails);
     };
+    
 
     const Add_Product = async () => {
         if (!image) {
