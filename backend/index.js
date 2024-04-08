08fd89fb7b507792d32a1982b3da4c5dca1432e6
Aadil_Tansawala <aadil882002@gmail.com>
2024-04-09 04:41:35 +0530

    stripe api-noe-fetch

--- backend/index.js
+++ backend/index.js
@@ -686,7 +686,7 @@ app.post('/create-payment-intent', async (req, res) => {
                 // Construct the product data object
                 const productData = {
                     name: item.name,
-                    images: [imageUrl], // Pass image URL as an array
+                    images: [imageResponse], // Pass image URL as an array
                 };
     
                 return {
